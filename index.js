437586c55b6febdab788e323214f6184b72f194d
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2022-11-27 09:58:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ console.log(`
 
 Sonu Kumar Kushwaha  
 
-I'm Sonu kumar kushwaha from India, born in Gopalganj, a sunny place in the state of Bihar. A computer science student, Passionate reader. Eager to find the root cause of problems. For fun, I like to ride my motorcycle and follow my home cricket team.
+I'm Sonu kumar kushwaha from India, born in Gopalganj, a sunny place in the state of Bihar. A computer science student, Passionate reader. 
+Eager to find the root cause of problems. For fun, I like to ride my motorcycle and follow my home cricket team.
 
 
 If you just want to email me, go ahead send an email to flyingsonu736@gmail.com but be nice and make sure you Googled around a bit before you do. ;)
@@ -12,8 +13,8 @@ If you just want to email me, go ahead send an email to flyingsonu736@gmail.com 
 That's me, today. I'll be different tomorrow, hopefully better.
 
 
-🐤 Twitter:     https://twitter.com/sonukumarkush12
+🐤 Twitter:     https://twitter.com/iamsonukushwaha
 📖 GitHub:      https://github.com/flyingsonu122
-😎 Portfolio:   https://sonukushwaha.live
+😎 Portfolio:   https://sonukushwaha.me
 
 `);
