7d0ab22010adc4eac359fb8538d0b58355b33194
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2023-05-25 02:00:41 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -30,11 +30,10 @@ $ npx sonukushwaha
 
 **MIT &copy; [Sonu kumar kushwaha](https://singlebucks.blogspot.com/)**
 
-## Oh, Thanks!
+
 
 Thank you for being here! You can find my other projects **[here](https://github.com/flyingsonu122?tab=repositories)**.
 
-[![Built with Love](https://forthebadge.com/images/badges/built-with-love.svg)](https://linktr.ee/flyingsonu) 
 
 <br><br>
 <p align="center"> Let's have a chat on </p> 
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 #!/usr/bin/env node
 
-console.log(`
+import chalk from 'chalk';
 
-Hi
+console.log(chalk.blue(`Hi
 
-I'm Sonu kumar kushwaha from India, born in Gopalganj, a sunny place in the state of Bihar. A computer science student, Passionate reader. 
+I'm ${chalk.rgb(255, 136, 0).bold('Sonu Kumar Kushwaha')} from India, born in Gopalganj, a sunny place in the state of Bihar. A computer science student, Passionate reader. 
 
 Eager to find the root cause of problems. For fun, I like to ride my motorcycle and follow my home cricket team.
 
 
-If you just want to email me, go ahead send an email to flyingsonu736@gmail.com but be nice and make sure you Googled around a bit before you do. ;)
+If you just want to email me, go ahead send an email to ${chalk.green('flyingsonu736@gmail.com')} but be nice and make sure you Googled around a bit before you do. ;)
 
-That's me, today. I'll be different tomorrow, hopefully better.
+${chalk.white("That's me, today. I'll be different tomorrow, hopefully better.")}
 
-😎 Portfolio:   https://singlebucks.blogspot.com
+😎 Portfolio:  ${chalk.yellow('https://singlebucks.blogspot.com')}
 
-🐤 Twitter:     https://twitter.com/iamsonukushwaha
+🐤 Twitter:    ${chalk.yellow('https://twitter.com/iamsonukushwaha')}
 
-📖 GitHub:      https://github.com/flyingsonu122
+📖 GitHub:     ${chalk.yellow('https://github.com/flyingsonu122')}
 
-
-
-`);
+`));
